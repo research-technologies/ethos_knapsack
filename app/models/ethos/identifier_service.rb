64d11176b469cf2bfc90ab3f038c9f35df1dd209
Rory McNicholl <rory.mcnicholl@london.ac.uk>
2025-11-06 23:40:13 +0000

    create a sequential numeric ID, hoping that Rails.cache will keep it honest

--- app/models/ethos/identifier_service.rb
+++ app/models/ethos/identifier_service.rb
@@ -1,26 +1,27 @@
 require 'securerandom'
 
 module Ethos
-  # IdentifierService should be mixed into AMS models that should have an
-  # AMS-style ID. For instance the Asset model in app/models/asset.rb should
-  # `include IdentifierService` in order to get IDs produced by this module.
+  # Absolutely insistent on sequential numeric IDs, these are inherently risky,
+  # but they won't listen...
   #
   # NOTE: The module should be included AFTER the Hyrax::WorkBehavior module
   # is included, because this module overrides the #assign_id method that
   # comes from including Hyrax::WorkBehavior.
   module IdentifierService
-    ID_PREFIX = "stuck-to-the-front-"
 
     # mint
-    # Mints a new ID for use in Fedora/Solr of new records, which is an ID that
-    # has the constant ID_PREFIX prepended to a 12 character subset of a UUID.
-    # @return [String] a new random ID prefixed with constant
-    #  AMS::IdentifierService::ID_PREFIX that does not already exist in Solr.
+    # Mints a new ID for use in Fedora/Postgres/Solr that is a sequential number
+    # We use Rails cache to store the last one created and we increment after we access it
+    # This number will only go up, it won't revert or decrease should a) the object not 
+    # end up getting created or b) the object is deleted... In some small way, we have 
+    # just made Hyrax into EPrints
+    # @return [String] a new ID
     def self.mint
       begin
-        new_id = ID_PREFIX + SecureRandom.uuid.tr('-', '').slice(0, 11)
-      end until usable_id? new_id
-      new_id
+        new_id = Rails.cache.fetch("thesis_id") { 1 }
+        Rails.cache.write("thesis_id", (new_id+1) )
+      end until usable_id? new_id 
+      new_id.to_s
     end
 
     def self.usable_id?(id)
