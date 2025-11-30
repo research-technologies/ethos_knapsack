4920b2b68d1d990b4bce0398036fc23a4b2021e4
Rory McNicholl <rory.mcnicholl@london.ac.uk>
2025-11-30 21:41:38 +0000

    make an ethos_identifier when we aren't given one (i.e. new works)

--- lib/valkyrie/persistance/postgres/resource_converter_decorator.rb
+++ lib/valkyrie/persistance/postgres/resource_converter_decorator.rb
@@ -14,7 +14,7 @@ module Valkyrie
           # so as if ethos_identifer is present but references an existing object...
           # it should pick that up from resource.id for existing items
           # and also if we arrive from the form our ethos_identifier (source_identifier) is nil or a singleton
-          # if we arrive from an import our ethos_identifier is in an array and if not passed in has _already_ been soert of minted
+          # if we arrive from an import our ethos_identifier is in an array and if not passed in has _already_ been sort of minted
           # by the fill_in_blank_source_identifier lamda in the bulkrax config
           ethos_identifier = resource.ethos_identifier.is_a?(Array) ? resource.ethos_identifier.first : resource.ethos_identifier
           current_id = if ethos_identifier.nil?
@@ -22,6 +22,7 @@ module Valkyrie
                        else
                          resource.id || ethos_identifier.gsub("uk.bl.ethos.", "")
                        end
+          resource.ethos_identifier = "uk.bl.ethos.#{current_id}" if ethos_identifier.nil?
           orm_class.find_or_initialize_by(id: current_id.to_s).tap do |orm_object|
             orm_object.internal_resource = resource.internal_resource
             process_lock_token(orm_object)
