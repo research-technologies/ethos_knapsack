eeb004923d553a2de267145a57b6adc2308328a4
Rory McNicholl <rory.mcnicholl@london.ac.uk>
2025-12-07 22:23:09 +0000

    ensure all fields display in work page, also add string indexing for dois and ids etc

--- config/initializers/hyrax.rb
+++ config/initializers/hyrax.rb
@@ -21,8 +21,8 @@ Qa::Authorities::Local.register_subauthority('contributor_roles', 'Qa::Authoriti
 require 'oai/provider/metadata_format/uketd_dc'
 OAI::Provider::Base.register_format(OAI::Provider::Metadata::UketdDc.instance)
 
+# rubocop:disable Metrics/BlockLength
 SolrDocument.class_eval do
-
   use_extension(Blacklight::Document::UketdDc)
   def to_uketd_dc
     export_as('uketd_dc_xml')
@@ -47,13 +47,11 @@ SolrDocument.class_eval do
   attribute :ethos_subject, Hyrax::SolrDocument::Metadata::Solr::Array, 'ethos_subject_tesim'
   attribute :dewey, Hyrax::SolrDocument::Metadata::Solr::Array, 'dewey_tesim'
   attribute :ethos_access_rights, Hyrax::SolrDocument::Metadata::Solr::Array, 'ethos_access_rights_tesim'
-  attribute :embargo_date, Hyrax::SolrDocument::Metadata::Solr::Array, 'embargo_date_tesim'
-  attribute :doi, Hyrax::SolrDocument::Metadata::Solr::Array, 'doi_tesim'
-  attribute :referenced_by, Hyrax::SolrDocument::Metadata::Solr::Array, 'referenced_by_tesim'
-  attribute :oai_identifier, Hyrax::SolrDocument::Metadata::Solr::Array, 'oai_identifier_tesim'
-  attribute :source_record, Hyrax::SolrDocument::Metadata::Solr::Array, 'source_record_tesim'
+  attribute :embargo_date, Hyrax::SolrDocument::Metadata::Solr::Array, 'embargo_date_ssim'
+  attribute :doi, Hyrax::SolrDocument::Metadata::Solr::Array, 'doi_ssim'
+  attribute :referenced_by, Hyrax::SolrDocument::Metadata::Solr::Array, 'referenced_by_ssim'
+  attribute :oai_identifier, Hyrax::SolrDocument::Metadata::Solr::Array, 'oai_identifier_ssim'
   attribute :licence, Hyrax::SolrDocument::Metadata::Solr::Array, 'licence_tesim'
-
 end
 
 ::CatalogController.class_eval do
@@ -107,26 +105,81 @@ end
   blacklight_config.add_show_field 'dewey_tesim'
   blacklight_config.add_show_field 'ethos_access_rights_tesim'
   blacklight_config.add_show_field 'embargo_date_tesim'
-  blacklight_config.add_show_field 'doi_tesim'
-  blacklight_config.add_show_field 'referenced_by_tesim'
-  blacklight_config.add_show_field 'oai_identifier_tesim'
-  blacklight_config.add_show_field 'source_record_tesim'
+  blacklight_config.add_show_field 'doi_ssim'
+  blacklight_config.add_show_field 'referenced_by_ssim'
+  blacklight_config.add_show_field 'oai_identifier_ssim'
   blacklight_config.add_show_field 'licence_tesim'
 
-  # Adavnaced search fields
-  blacklight_config.add_search_field('qualification_name') do |field|
-      field.solr_parameters = {
-        "spellcheck.dictionary": "qualification_name"
-      }
-      solr_name = 'qualification_name_tesim'
-      field.solr_local_parameters = {
-        qf: solr_name,
-        pf: solr_name
-      }
-    end
+  # This one uses all the defaults set by the solr request handler. Which
+  # solr request handler? The one set in config[:default_solr_parameters][:qt],
+  # since we aren't specifying it otherwise.
+  blacklight_config.search_fields.delete(:all_fields)
+  # Re-add the "simple" search fields now we have rejigged all the show fields
+  blacklight_config.add_search_field('all_fields', label: 'All Fields', include_in_advanced_search: false) do |field|
+    all_names = blacklight_config.show_fields.values.map(&:field).join(" ")
+    title_name = 'title_tesim'
+    field.solr_parameters = {
+      qf: "#{all_names} #{title_name} id file_format_tesim all_text_tsimv all_text_tsimv",
+      pf: title_name.to_s
+    }
+  end
 
+  # Adavnaced search fields
+
+  blacklight_config.add_search_field('doi') do |field|
+    field.solr_parameters = {
+      "spellcheck.dictionary": "doi"
+    }
+    solr_name = 'doi_ssim'
+    field.solr_local_parameters = {
+      qf: solr_name,
+      pf: solr_name
+    }
+  end
+
+  blacklight_config.add_search_field('abstract') do |field|
+    field.solr_parameters = {
+      "spellcheck.dictionary": "abstract"
+    }
+    solr_name = 'abstract_tesim'
+    field.solr_local_parameters = {
+      qf: solr_name,
+      pf: solr_name
+    }
+  end
+
+  blacklight_config.add_search_field('qualification_name') do |field|
+    field.solr_parameters = {
+      "spellcheck.dictionary": "qualification_name"
+    }
+    solr_name = 'qualification_name_tesim'
+    field.solr_local_parameters = {
+      qf: solr_name,
+      pf: solr_name
+    }
+  end
+end
+# rubocop:enable Metrics/BlockLength
+
+# Overrides Hyku WorkShowPresenter
+Hyku::WorkShowPresenter.class_eval do
+  def doi
+    doi_regex = %r{10\.\d{4,9}\/[-._;()\/:A-Z0-9]+}i
+    doi = extract_from_identifier(doi_regex)
+    doi&.join
+  end
+
+  def extract_from_identifier(rgx)
+    if solr_document['doi_ssim'].present?
+      ref = solr_document['doi_ssim'].map do |str|
+        str.scan(rgx)
+      end
+    end
+    ref
+  end
 end
 
+# Overrides blacklight_oai_provider
 BlacklightOaiProvider::SolrSet.class_eval do
   def self.sets_from_facets(facet_results)
     sets = Array.wrap(@fields).map do |f|
