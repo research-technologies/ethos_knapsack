589482016a34a5163babac6010d7017581503f29
Rory McNicholl <rory.mcnicholl@london.ac.uk>
2026-01-20 12:31:26 +0000

    need to use _ssi for ethos_identifier lookup in controller... will require a re-index

--- app/controllers/hyku_knapsack/original_id_controller.rb
+++ app/controllers/hyku_knapsack/original_id_controller.rb
@@ -3,7 +3,7 @@ module HykuKnapsack
   class OriginalIdController < ApplicationController
     def show
       original_id = params[:uin]
-      hit = ::Hyrax::SolrService.query("ethos_identifier_tesim:#{original_id}", rows: 1, fl: 'id,has_model_ssim').first
+      hit = ::Hyrax::SolrService.query("ethos_identifier_ssi:#{original_id}", rows: 1, fl: 'id,has_model_ssim').first
       return redirect_to hyrax.root_path, alert: "Item with identifier #{params[:uin]} not found." if hit.blank?
 
       id = hit['id']
