81961a8fa21b79671963f8c7e40c6422130be7ce
Rory McNicholl <rory.mcnicholl@london.ac.uk>
2026-01-20 11:18:38 +0000

    while we are here... also fix the old link controller

--- app/controllers/hyku_knapsack/original_id_controller.rb
+++ app/controllers/hyku_knapsack/original_id_controller.rb
@@ -1,20 +1,11 @@
 # frozen_string_literal: true
 module HykuKnapsack
   class OriginalIdController < ApplicationController
+
     def show
-      original_id = params[:id]
-      hit = ::Hyrax::SolrService.query("ethos_identifier_ssi:#{original_id}", rows: 1, fl: 'id,has_model_ssim').first
-      return redirect_to root_path, alert: "Item with identifier #{params[:id]} not found." if hit.blank?
-
-      id = hit['id']
-      model = hit['has_model_ssim'].first
-      redirect_to "/concern/#{model.underscore.pluralize}/#{id}"
-    end
-
-    def show2
       original_id = params[:uin]
-      hit = ::Hyrax::SolrService.query("ethos_identifier_ssi:#{original_id}", rows: 1, fl: 'id,has_model_ssim').first
-      return redirect_to root_path, alert: "Item with identifier #{params[:uin]} not found." if hit.blank?
+      hit = ::Hyrax::SolrService.query("ethos_identifier_tesim:#{original_id}", rows: 1, fl: 'id,has_model_ssim').first
+      return redirect_to hyrax.root_path, alert: "Item with identifier #{params[:uin]} not found." if hit.blank?
 
       id = hit['id']
       model = hit['has_model_ssim'].first
