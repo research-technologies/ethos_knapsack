8775e2708205e31f5b80bd2a515829415a769287
Rory McNicholl <rory.mcnicholl@london.ac.uk>
2026-06-17 08:23:59 +0100

    introduce and use FAQs route for help page...

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,5 @@ end
 
 Rails.application.routes.draw do
   get 'contact_us' => 'hyrax/pages#show', key: 'contact_us'
-  get 'FAQs' => 'hyrax/help#show', key: 'help'
+  get 'FAQs' => 'hyrax/pages#show', key: 'help'
 end
